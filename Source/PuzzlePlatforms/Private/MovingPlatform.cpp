643832ef1f683a73cc6894cbb2285bf6ea0a4f32
Konsta Kontunen <konsta.kontunen@ubisoft.com>
2019-10-11 17:39:52 +0300

    Authority and Replication

---
+++ Source/PuzzlePlatforms/Private/MovingPlatform.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MovingPlatform.h"
+
+
+AMovingPlatform::AMovingPlatform()
+{
+
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SetMobility(EComponentMobility::Movable);
+}
+
+// Called when the game starts or when spawned
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HasAuthority())
+	{
+		SetReplicates(true);
+		SetReplicateMovement(true);
+	}
+
+}
+
+// Called every frame
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (HasAuthority()) 
+	{
+		FVector Location = GetActorLocation();
+		Location += FVector(Speed * DeltaTime, 0, 0);
+		SetActorLocation(Location);
+	}
+}
