b08050940312be0376e330628f13e1a1b29f57a7
Haamuprikaati <konsta.kontunen@gmail.com>
2019-10-12 03:01:00 +0300

    Hosting Servers With Server Travel

--- Source/PuzzlePlatforms/Private/PuzzlePlatforms_GI.cpp
+++ Source/PuzzlePlatforms/Private/PuzzlePlatforms_GI.cpp
@@ -25,6 +25,11 @@ void UPuzzlePlatforms_GI::Host()
 	if (!ensure(Engine != nullptr)) return;
 
 	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, TEXT("Hosting"));
+
+	UWorld* World = GetWorld();
+	if (!ensure(World != nullptr)) return;
+
+	World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen");
 }
 
 void UPuzzlePlatforms_GI::Join(const FString& Address)
